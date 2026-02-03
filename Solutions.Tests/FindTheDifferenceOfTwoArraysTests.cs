1520a1122db58c5c8bc6158a9ecc6b81bdac0a4d
Sinan Ganiz <sinan.ganiz@gmail.com>
2026-02-03 19:45:35 +0300

    2215 solved

---
+++ Solutions.Tests/FindTheDifferenceOfTwoArraysTests.cs
@@ -0,0 +1,97 @@
+﻿using Solutions.FindTheDifferenceOfTwoArrays;
+
+namespace Solutions.Tests;
+
+public class FindTheDifferenceOfTwoArraysTests
+{
+    [Fact]
+    public void DifferenceOfTwoArraysTest1()
+    {
+        var solution = new Solution();
+        var result = solution.FindDifference(new int[] { 1, 2, 3 }, new int[] { 2, 4, 6 });
+        Assert.Equal(new List<int> { 1, 3 }, result[0].OrderBy(x => x).ToList());
+        Assert.Equal(new List<int> { 4, 6 }, result[1].OrderBy(x => x).ToList());
+    }
+
+    [Fact]
+    public void DifferenceOfTwoArraysTest2()
+    {
+        var solution = new Solution();
+        var result = solution.FindDifference(new int[] { 1, 2, 3, 3 }, new int[] { 1, 1, 2, 2 });
+        Assert.Equal(new List<int> { 3 }, result[0].OrderBy(x => x).ToList());
+        Assert.Empty(result[1]);
+    }
+
+    [Fact]
+    public void DifferenceOfTwoArraysTest3()
+    {
+        var solution = new Solution();
+        var result = solution.FindDifference(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 });
+        Assert.Empty(result[0]);
+        Assert.Empty(result[1]);
+    }
+
+    [Fact]
+    public void DifferenceOfTwoArraysTest4()
+    {
+        var solution = new Solution();
+        var result = solution.FindDifference(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 });
+        Assert.Equal(new List<int> { 1, 2, 3 }, result[0].OrderBy(x => x).ToList());
+        Assert.Equal(new List<int> { 4, 5, 6 }, result[1].OrderBy(x => x).ToList());
+    }
+
+    [Fact]
+    public void DifferenceOfTwoArraysTest5()
+    {
+        var solution = new Solution();
+        var result = solution.FindDifference(new int[] { 1, 1, 1, 1 }, new int[] { 2, 2, 2, 2 });
+        Assert.Equal(new List<int> { 1 }, result[0].OrderBy(x => x).ToList());
+        Assert.Equal(new List<int> { 2 }, result[1].OrderBy(x => x).ToList());
+    }
+
+    [Fact]
+    public void DifferenceOfTwoArraysTest6()
+    {
+        var solution = new Solution();
+        var result = solution.FindDifference(new int[] { -1, -2, -3 }, new int[] { -2, -4, -6 });
+        Assert.Equal(new List<int> { -3, -1 }, result[0].OrderBy(x => x).ToList());
+        Assert.Equal(new List<int> { -6, -4 }, result[1].OrderBy(x => x).ToList());
+    }
+
+    [Fact]
+    public void DifferenceOfTwoArraysTest7()
+    {
+        var solution = new Solution();
+        var result = solution.FindDifference(new int[] { 1 }, new int[] { 1 });
+        Assert.Empty(result[0]);
+        Assert.Empty(result[1]);
+    }
+
+    [Fact]
+    public void DifferenceOfTwoArraysTest8()
+    {
+        var solution = new Solution();
+        var result = solution.FindDifference(new int[] { 1 }, new int[] { 2 });
+        Assert.Equal(new List<int> { 1 }, result[0].OrderBy(x => x).ToList());
+        Assert.Equal(new List<int> { 2 }, result[1].OrderBy(x => x).ToList());
+    }
+
+    [Fact]
+    public void DifferenceOfTwoArraysTest9()
+    {
+        var solution = new Solution();
+        var result = solution.FindDifference(new int[] { 1, 2, 3, 4, 5 }, new int[] { 3, 4, 5, 6, 7 });
+        Assert.Equal(new List<int> { 1, 2 }, result[0].OrderBy(x => x).ToList());
+        Assert.Equal(new List<int> { 6, 7 }, result[1].OrderBy(x => x).ToList());
+    }
+
+    [Fact]
+    public void DifferenceOfTwoArraysTest10()
+    {
+        var solution = new Solution();
+        var result = solution.FindDifference(new int[] { 0, 0, 0 }, new int[] { 0, 0, 0 });
+        Assert.Empty(result[0]);
+        Assert.Empty(result[1]);
+    }
+
+}
---
+++ Solutions/2215 - Find the Difference of Two Arrays/FindTheDifferenceOfTwoArrays.cs
@@ -0,0 +1,34 @@
+﻿namespace Solutions.FindTheDifferenceOfTwoArrays;
+
+public class Solution
+{
+    public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+    {
+        HashSet<int> set1 = new HashSet<int>(nums1);
+        HashSet<int> set2 = new HashSet<int>(nums2);
+        
+        HashSet<int> uniqueNums1 = new HashSet<int>();
+        foreach (int num in set1)
+        {
+
+            if (!set2.Contains(num))
+            {
+                uniqueNums1.Add(num);
+            }
+        }
+
+        HashSet<int> uniqueNums2 = new HashSet<int>();
+        foreach (int num in set2)
+        {
+            if (!set1.Contains(num))
+            {
+                uniqueNums2.Add(num);
+            }
+        }
+        var result = new List<IList<int>>();
+        result.Add(uniqueNums1.ToList());
+        result.Add(uniqueNums2.ToList());
+
+        return result;
+    }
+}
