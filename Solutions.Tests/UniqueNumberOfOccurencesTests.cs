6e23271f3f99183b21b00551fc84d05415414a99
Sinan Ganiz <sinan.ganiz@gmail.com>
2026-02-03 20:01:16 +0300

    1207 solved

---
+++ Solutions.Tests/UniqueNumberOfOccurencesTests.cs
@@ -0,0 +1,86 @@
+﻿using Solutions.UniqueNumberOfOccurences;
+
+namespace Solutions.Tests;
+
+public class UniqueNumberOfOccurencesTests
+{
+    [Fact]
+    public void UniqueOccurrencesTest1()
+    {
+        var solution = new Solution();
+        var result = solution.UniqueOccurrences(new int[] { 1, 2, 2, 1, 1, 3 });
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void UniqueOccurrencesTest2()
+    {
+        var solution = new Solution();
+        var result = solution.UniqueOccurrences(new int[] { 1, 2 });
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void UniqueOccurrencesTest3()
+    {
+        var solution = new Solution();
+        var result = solution.UniqueOccurrences(new int[] { -3, 0, 1, -3, 1, 1, 1, -3, 10, 0 });
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void UniqueOccurrencesTest4()
+    {
+        var solution = new Solution();
+        var result = solution.UniqueOccurrences(new int[] { 1 });
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void UniqueOccurrencesTest5()
+    {
+        var solution = new Solution();
+        var result = solution.UniqueOccurrences(new int[] { 1, 1, 1, 1 });
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void UniqueOccurrencesTest6()
+    {
+        var solution = new Solution();
+        var result = solution.UniqueOccurrences(new int[] { 1, 2, 3 });
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void UniqueOccurrencesTest7()
+    {
+        var solution = new Solution();
+        var result = solution.UniqueOccurrences(new int[] { -1, -1, 1, 1, 2 });
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void UniqueOccurrencesTest8()
+    {
+        var solution = new Solution();
+        var result = solution.UniqueOccurrences(new int[] { 0, 0, 0, 0, 0 });
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void UniqueOccurrencesTest9()
+    {
+        var solution = new Solution();
+        var result = solution.UniqueOccurrences(new int[] { -1000, -1000, 1000, 1000 });
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void UniqueOccurrencesTest10()
+    {
+        var solution = new Solution();
+        var result = solution.UniqueOccurrences(new int[] { 1, 2, 2, 3, 3, 3, 4, 4, 4, 4 });
+        Assert.True(result);
+    }
+}
---
+++ Solutions/1207 - Unique Number of Occurrences/UniqueNumberOfOccurences.cs
@@ -0,0 +1,19 @@
+﻿namespace Solutions.UniqueNumberOfOccurences;
+
+public class Solution
+{
+    public bool UniqueOccurrences(int[] arr)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        foreach (int num in arr)
+        {
+            dict.TryGetValue(num, out int count);
+            dict[num] = count + 1;
+        }
+
+        var occurencesArr = dict.Values.ToArray();
+        HashSet<int> occurencesUniq = new HashSet<int>(occurencesArr);
+
+        return occurencesUniq.Count == occurencesArr.Length;
+    }
+}
