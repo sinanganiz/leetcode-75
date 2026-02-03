9b10a5945bf9eed6ab0bf3e59e5427cc21476753
Sinan Ganiz <sinan.ganiz@gmail.com>
2026-02-03 20:15:11 +0300

    2390 solved

---
+++ Solutions.Tests/RemovingStarsFromAStringTests.cs
@@ -0,0 +1,78 @@
+﻿using Solutions.RemovingStarsFromAString;
+
+namespace Solutions.Tests;
+
+public class RemovingStarsFromAStringTests
+{
+    [Fact]
+    public void RemovingStarsTest1()
+    {
+        var solution = new Solution();
+        var result = solution.RemoveStars("leet**cod*e");
+        Assert.Equal("lecoe", result);
+    }
+
+    [Fact]
+    public void RemovingStarsTest2()
+    {
+        var solution = new Solution();
+        var result = solution.RemoveStars("erase*****");
+        Assert.Equal("", result);
+    }
+
+    [Fact]
+    public void RemovingStarsTest3()
+    {
+        var solution = new Solution();
+        var result = solution.RemoveStars("abc");
+        Assert.Equal("abc", result);
+    }
+
+    [Fact]
+    public void RemovingStarsTest4()
+    {
+        var solution = new Solution();
+        var result = solution.RemoveStars("a*");
+        Assert.Equal("", result);
+    }
+
+    [Fact]
+    public void RemovingStarsTest5()
+    {
+        var solution = new Solution();
+        var result = solution.RemoveStars("ab*c");
+        Assert.Equal("ac", result);
+    }
+
+    [Fact]
+    public void RemovingStarsTest6()
+    {
+        var solution = new Solution();
+        var result = solution.RemoveStars("a*b*c*");
+        Assert.Equal("", result);
+    }
+
+    [Fact]
+    public void RemovingStarsTest7()
+    {
+        var solution = new Solution();
+        var result = solution.RemoveStars("abc***");
+        Assert.Equal("", result);
+    }
+
+    [Fact]
+    public void RemovingStarsTest8()
+    {
+        var solution = new Solution();
+        var result = solution.RemoveStars("abcde");
+        Assert.Equal("abcde", result);
+    }
+
+    [Fact]
+    public void RemovingStarsTest9()
+    {
+        var solution = new Solution();
+        var result = solution.RemoveStars("ab**cd**ef");
+        Assert.Equal("ef", result);
+    }
+}
---
+++ Solutions/2390 - Removing Stars From a String/RemovingStarsFromAString.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Solutions.RemovingStarsFromAString;
+
+public class Solution
+{
+    public string RemoveStars(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        foreach (char c in s)
+        {
+            if (c != '*')
+                stack.Push(c);
+            else
+                stack.Pop();
+        }
+
+        return new string(stack.Reverse().ToArray());
+    }
+}
